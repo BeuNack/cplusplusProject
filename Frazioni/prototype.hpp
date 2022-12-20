45ff1bdb2f8c162cd4d8b8f32e17e53c91eca771
BeuNack <90098910+BeuNack@users.noreply.github.com>
2022-12-20 01:46:09 +0100

    Create prototype.hpp

---
+++ Frazioni/prototype.hpp
@@ -0,0 +1,12 @@
+#ifndef PROTOTIPI_HPP_INCLUDED
+#define PROTOTIPI_HPP_INCLUDED
+
+void somma(int &den, int &den2, int &num, int &num2);
+void sottrazione(int &den, int &den2, int &num, int &num2);
+void moltiplicazione(int &den, int &den2, int &num, int &num2);
+void divisione(int &den, int &den2, int &num, int &num2);
+void potenze(int &den, int &num, int esponente);
+void riduciFrazione(int &num, int &den);
+
+
+#endif // PROTOTIPI_HPP_INCLUDED
